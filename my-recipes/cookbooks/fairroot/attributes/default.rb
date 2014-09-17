718b168da848ebd8bac88f8b86c83d05a8bd9dd4
tkollegg <Thorsten.Kollegger@cern.ch>
2014-09-17 12:49:37 +0200

    Some more generic cleanup to get a properly set-up machine.

---
+++ my-recipes/cookbooks/fairroot/attributes/default.rb
@@ -0,0 +1,3 @@
+default[:fairsoft][:dir] = "/opt/fairsoft"
+default[:fairsoft][:version] = "jul14p3"
+
--- my-recipes/cookbooks/fairroot/recipes/fairsoft.rb
+++ my-recipes/cookbooks/fairroot/recipes/fairsoft.rb
@@ -1,15 +1,12 @@
-# variables
-version = "jul14p3"
-install_dir = "/opt/fairsoft"
 
-directory install_dir do
+directory node[:fairsoft][:dir] do
   owner "vagrant"
   group "vagrant"
   mode 0755
   action :create
 end
 
-directory install_dir+"/"+version do
+directory node[:fairsoft][:dir]+"/"+node[:fairsoft][:version] do
   owner "vagrant"
   group "vagrant"
   mode 00755
@@ -18,24 +15,34 @@ end
 
 git "/home/vagrant/fairsoft" do
   repository "https://github.com/FairRootGroup/FairSoft"
-  revision "tags/" + version
+  revision "tags/" + node[:fairsoft][:version]
   action :sync
   user "vagrant"
   group "vagrant"
 end
 
+template "fairsoft.sh" do
+  path "/etc/profile.d/fairsoft.sh"
+  source "fairsoft.sh.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "config.input" do
+  path "/home/vagrant/fairsoft/config.input"
+  source "config.input.erb"
+  owner "vagrant"
+  group "vagrant"
+  mode "0644"
+end
+
 bash "install_fairsoft" do
   user "vagrant"
   cwd "/home/vagrant/fairsoft"
+  timeout 7200
   code <<-EOH
-    ./configure.sh << EOF
-    1
-    1
-    2
-    2
-    /opt/fairsoft/jul14p3
-    2
-    EOF
+    ./configure.sh < config.input
   EOH
 end
 
