156760ad6060f05110e757207174a34d96e8ae36
tkollegg <Thorsten.Kollegger@cern.ch>
2014-09-30 10:52:56 +0200

    enable fairroot

--- my-recipes/cookbooks/fairroot/attributes/default.rb
+++ my-recipes/cookbooks/fairroot/attributes/default.rb
@@ -3,4 +3,8 @@ default[:fairsoft][:version] = "jul14p3"
 default[:fairsoft][:src_dir] = "/home/vagrant"
 default[:fairsoft][:src_keep] = true
 
+default[:fairroot][:dir] = "/opt/fairroot"
+default[:fairroot][:src_dir] = "/home/vagrant"
+default[:fairroot][:version] = "dev"
+
 
